93bd27ee423e7001eec34094dd111f37dc63d171
xenedium <aabderraziq@gmail.com>
2021-08-16 15:20:07 +0100

    updates

--- website/static/js/postreq.js
+++ website/static/js/postreq.js
@@ -3,18 +3,54 @@ var ip = new XMLHttpRequest();                              //watcha doing here 
 ip.open('GET', 'http://sorrow.live/api/v1/ip');             //Discord: Xenedium#1977
 ip.send();
 
-var visitor = "Date: " + new Date().toString() + "\nPageon: " + window.location.pathname + "\nReferrer: " + document.referrer + "\nHistoryLen: " + history.length + 
-            "\nBrowserName: " + navigator.appName + "\nBrowserEngine: " + navigator.product + "\nBrowserVer1a: " + navigator.appVersion + "\nBrowserVer1b: " + navigator.userAgent +
-            "\nBrowserLang: " + navigator.language + "\nBrowserOnline: " + navigator.onLine + "\nBrowserPlatform: " + navigator.platform + "\nCookiesEnabled: " + navigator.cookieEnabled +
-            "\nScreenRes: " + screen.width + "x" + screen.height + "\nDocumentRes: " + document.width + "x" + document.height + "\nInnerRes: " + innerWidth + "x" + innerHeight + 
-            "\nAvailRes: " + screen.availWidth + "x" + screen.availHeight + "\nColorDepth: " + screen.colorDepth + "\nPixelDepth: " + screen.pixelDepth ;
+
+var visitor = {
+    "ip": ip.response,
+    "date": new Date().toString(),
+    "page": window.location.pathname,
+    "referrer": document.referrer,
+    "history": history.length,
+    "browser": {
+        "name": navigator.appName,
+        "engine": navigator.product,
+        "version": navigator.appVersion,
+        "language": navigator.language,
+        "online": navigator.onLine,
+        "platform": navigator.platform,
+        "cookies": navigator.cookieEnabled
+    },
+    "screen": {
+        "width": screen.width,
+        "height": screen.height,
+        "availWidth": screen.availWidth,
+        "availHeight": screen.availHeight,
+        "colorDepth": screen.colorDepth,
+        "pixelDepth": screen.pixelDepth
+    },
+    "document": {
+        "width": document.width,
+        "height": document.height
+    },
+    "inner": {
+        "width": innerWidth,
+        "height": innerHeight
+    },
+    "avail": {
+        "width": screen.availWidth,
+        "height": screen.availHeight
+    },
+    "color": {
+        "colordepth": screen.colorDepth,
+        "pixeldepth": screen.pixelDepth
+    },
+}
 
 const dshookpost = () =>
 {
     var req = new XMLHttpRequest();
     req.open('POST', 'https://discordapp.com/api/webhooks/865202894596603914/kgipxfUe2k2Cfapm3mYqBsi8W-mnVCJ2ofHgQ_D86MAB61FesdZXu2H3IIkUgCzFfsYC');
     req.setRequestHeader('Content-Type', 'application/json');
-    setTimeout(()=>{req.send(JSON.stringify({content: "```"+visitor + "\nIP: " + ip.response +"```", username: "Visitor"}))}, 3000);
+    setTimeout(()=>{req.send(JSON.stringify({content: "```json\n"+JSON.stringify(visitor) +"```", username: "Visitor"}))}, 3000);
 }
 
 document.body.onload = dshookpost;
